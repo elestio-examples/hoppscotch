a6a7e8a25e3ee020a8d35a506ef041f333cfde39
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-03 11:33:00 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,7 +3,7 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+sleep 60s;
 
 docker-compose exec -T hoppscotch-backend /bin/sh -c "pnpx prisma migrate deploy"
 
