f3d33fdc55bcbe6d7e0c07a77a09a89b038d69a8
Prem chavhan <pchavhan@logiquad.com>
2023-10-05 20:08:43 +0530

    fixes

--- tests/admin/build.sh
+++ tests/admin/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-rm package.json
-cp -rf ./packages/hoppscotch-sh-admin/* ./
+cp -rf ./packages/hoppscotch-sh-admin/Dockerfile ./
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
 docker buildx build . --output type=docker,name=elestio4test/hoppscotch-admin:latest | docker load
