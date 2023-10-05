92c26e20c6d1c3d7d675dd5eb07140c1af41fb75
Prem chavhan <pchavhan@logiquad.com>
2023-10-05 20:33:26 +0530

    fixes

--- tests/web/build.sh
+++ tests/web/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp -rf ./packages/hoppscotch-selfhost-web/* ./
+cp -rf ./packages/hoppscotch-selfhost-web/Dockerfile ./
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
 docker buildx build . --output type=docker,name=elestio4test/hoppscotch-app:latest | docker load
