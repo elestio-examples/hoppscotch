690558e7fe1a27453df533f93c0432a897f9ca51
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-18 13:41:12 +0300

    fix

--- tests/sh-admin/build.sh
+++ tests/sh-admin/build.sh
@@ -2,4 +2,4 @@
 cp -rf ./packages/hoppscotch-sh-admin/Dockerfile ./
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
-docker buildx build . --output type=docker,name=elestio4test/hoppscotch-admin:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/hoppscotch-sh-admin:latest | docker load
