f873518a7c98d16e82cb03f7603882134a60e2c6
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-23 10:52:47 +0300

    fix

--- tests/app/build.sh
+++ tests/app/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cp -rf ./packages/hoppscotch-selfhost-web/Dockerfile ./
+# cp -rf ./packages/hoppscotch-selfhost-web/Dockerfile ./
 rm -f docker-compose.yml
+cp ./prod.Dockerfile ./Dockerfile
 mv docker-compose-new.yml docker-compose.yml
-docker buildx build . --output type=docker,name=elestio4test/hoppscotch-app:latest | docker load
+docker buildx build . --target app --output type=docker,name=elestio4test/hoppscotch-app:latest | docker load
