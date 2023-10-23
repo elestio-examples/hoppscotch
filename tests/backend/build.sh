00108b0798fa2f437dd8c408be94a11e2be82178
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-23 15:08:37 +0300

    fix

--- tests/backend/build.sh
+++ tests/backend/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cp -rf ./packages/hoppscotch-backend/* ./
+# cp -rf ./packages/hoppscotch-backend/* ./
+cp ./prod.Dockerfile ./Dockerfile
 rm -f docker-compose.yml
 mv docker-compose-new.yml docker-compose.yml
-docker buildx build . --output type=docker,name=elestio4test/hoppscotch-backend:latest | docker load
+docker buildx build . --target backend --output type=docker,name=elestio4test/hoppscotch-backend:latest | docker load
